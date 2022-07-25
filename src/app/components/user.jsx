46ab950305e808e262aa6eddb63d8bdd0bbda52b
Bodnar S <serg_bod@ukr.net>
2022-07-25 20:12:02 +0300

    correction mistake

--- src/app/components/user.jsx
+++ src/app/components/user.jsx
@@ -3,7 +3,7 @@ import Qualitie from "./qualitie";
 import BookMark from "./bookmark";
 
 const User = ({user, ...rest}) => {
-    console.log("user =",user);
+    // console.log("user =",rest);
     return (
             <tr>
                     
@@ -25,9 +25,9 @@ const User = ({user, ...rest}) => {
                     <td>{user.rate}/5</td>
                     <td>
                         <BookMark
-                             status="true"
-                            // id={user._id}
-                            // {...rest}
+                            id={user._id}
+                            status={user.bookmark}
+                            {...rest}
                         />
                     </td>
                     <td><button type="button" className="btn btn-danger" onClick={() => rest.onDelete(user._id)}>delete</button></td>
